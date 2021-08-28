aa5cda1cca9eb61666ff916296d89454f2bf3abd
Christian Nwachukwu <christian@Christians-MacBook-Pro.local>
2021-08-28 10:49:57 -0400

    Renamed the res and req

--- routes/users.js
+++ routes/users.js
@@ -2,37 +2,37 @@ var express = require('express');
 var router = express.Router();
 
 /* GET: users listing. */
-router.get('/', function(req, res, next) {
-	res.render('./users/read-users', {title: 'Read Users'});
+router.get('/', function(request, response, next) {
+	response.render('./users/read-users', {title: 'Read Users'});
 });
 
-router.get('/:user-id', function(req, res, next) {
-	res.render('./users/list-users', {title: 'Single User'});
+router.get('/:user-id', function(request, response, next) {
+	response.render('./users/list-users', {title: 'Single User'});
 });
 
 /* GET: shows the create user form */
-router.get('/create', function(req, res, next) {
-	res.render('./users/create-user', {title: 'Create Users'});
+router.get('/create', function(request, response, next) {
+	response.render('./users/create-user', {title: 'Create Users'});
 });
 
 /* POST: Handles the submitted form */
-router.post('/create', function(req, res, next) {
-	res.send('Handle user creation');
+router.post('/create', function(request, response, next) {
+	response.send('Handle user creation');
 });
 
 /* SHOW UPDATE VIEW: Handles the updating view */
-router.get('/update/:id', function(req, res, next) {
-	res.render('./users/update-user', {title: 'Update [USER-NAME]'})
+router.get('/update/:id', function(request, response, next) {
+	response.render('./users/update-user', {title: 'Update [USER-NAME]'})
 });
 
 /* UPDATE: Handles the submitted form */
-router.put('/update/:id', function(req, res, next) {
-	res.send('Handle user creation');
+router.put('/update/:id', function(request, response, next) {
+	response.send('Handle user creation');
 });
 
 /* GET: Handles the submitted form */
-router.get('/delete/:id', function(req, res, next) {
-	res.send('Handle deleting users');
+router.get('/delete/:id', function(request, response, next) {
+	response.send('Handle deleting users');
 });
 
 module.exports = router;
